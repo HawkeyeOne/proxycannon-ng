62130e616110fabc2dc49f59df0b837006917942
hawkeyeone <ron@Desktop-Ron.localdomain>
2020-07-23 12:42:20 -0400

    Creating the Instance and related items

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
 resource "tls_private_key" "ssh" {
 	algorithm = "RSA"
 	rsa_bits = 4096
+	
+	provisioner "local-exec" {
+		command = "echo \"${tls_private_key.ssh.private_key_pem}\" > pc.pem"	
+	}
+	provisioner "local-exec" {
+		when = destroy
+		command = "rm ./pc.pem" 
+		on_failure = continue
+	}
 }
 
 resource "aws_key_pair" "generated_key" {
@@ -12,12 +21,43 @@ resource "aws_key_pair" "generated_key" {
 	public_key = tls_private_key.ssh.public_key_openssh
 }
 
+resource "aws_security_group" "allow_tls_ssh" {
+
+	name = "allow-tls-ssh" 
+	description = "Allow TLS/SSH inbound traffic"
+	
+	ingress {
+		description = "Inbound TLS"
+		from_port = 443
+		to_port = 443
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+
+	}
+	ingress {
+		description = "Inbound SSH"
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "proxycannon" 
+	}
+}
+
 data "aws_ami" "ubuntu" {
 	most_recent = true
 	
 	filter { 
 		name = "name"
-		values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+		values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
 	
 	}
 
@@ -32,9 +72,15 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "controller" {
 	ami = data.aws_ami.ubuntu.id
 	instance_type = "t2.micro" 
+	security_groups = ["allow-tls-ssh"]
 	key_name = aws_key_pair.generated_key.key_name
 
 	tags = {
 		Name = "Controller"
 	}
+}
+
+output "controller-ip" {
+	value = aws_instance.controller.public_ip
+	description = "Controller Public IP"
 }
