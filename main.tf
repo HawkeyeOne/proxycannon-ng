103dd70d8d69567824b94fc2a6ebea1f1ae2c73d
hawkeyeone <ron@Desktop-Ron.localdomain>
2020-07-23 08:27:29 -0400

    Addition of building controller instance

--- main.tf
+++ main.tf
@@ -11,3 +11,30 @@ resource "aws_key_pair" "generated_key" {
 	key_name = "pc-key"
 	public_key = tls_private_key.ssh.public_key_openssh
 }
+
+data "aws_ami" "ubuntu" {
+	most_recent = true
+	
+	filter { 
+		name = "name"
+		values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+	
+	}
+
+	filter {
+		name = "virtualization-type"
+		values = ["hvm"]
+	}
+
+	owners = ["099720109477"]
+}
+
+resource "aws_instance" "controller" {
+	ami = data.aws_ami.ubuntu.id
+	instance_type = "t2.micro" 
+	key_name = aws_key_pair.generated_key.key_name
+
+	tags = {
+		Name = "Controller"
+	}
+}
